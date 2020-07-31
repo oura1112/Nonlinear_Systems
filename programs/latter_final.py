e212c90b8bf87623313e1990f7e5bbb90a1c9fcf
oura1112 <59088115+oura1112@users.noreply.github.com>
2020-07-31 16:24:21 +0900

    0731

--- programs/latter_final.py
+++ programs/latter_final.py
@@ -9,9 +9,6 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 
-a = 1
-ips = 0.1
-
 #Pendulum witout friction
 class pendulum():
     
@@ -24,9 +21,18 @@ class pendulum():
         self.x2 = init_x2
         self.T = T
         
+        self.k = 3
+        self.m = 20
+        
     def pendulum(self,x1,x2):
         x1_dot = x2
         x2_dot = -self.a * math.sin(x1)
+        
+        return x1_dot, x2_dot
+    
+    def mass_spring(self,x1,x2):
+        x1_dot = x2
+        x2_dot = -x1
         
         return x1_dot, x2_dot
         
@@ -38,7 +44,7 @@ class pendulum():
         time = []
         H = []
         
-        H.append(self.x2**2/2 - self.a * math.cos(self.x1) + a)
+        H.append(self.x2**2/2 - self.a * math.cos(self.x1) + self.a)
         time.append(0)
         
         plt.scatter([self.x1], [self.x2], c="red", s=0.2)
@@ -53,7 +59,7 @@ class pendulum():
             H_x2.append(self.x2)
             
             time.append(t+1)
-            H.append(self.x2**2/2 - self.a * math.cos(self.x1) + a)
+            H.append(self.x2**2/2 - self.a * math.cos(self.x1) + self.a)
         
         plt.scatter([self.init_x1], [self.init_x2], c="red", s=20)
         plt.plot(H_x1, H_x2)
@@ -76,7 +82,7 @@ class pendulum():
         time = []
         H = []
         
-        H.append(self.x2**2/2 - self.a * math.cos(self.x1) + a)
+        H.append(self.x2**2/2 - self.a * math.cos(self.x1) + self.a)
         time.append(0)
         
         for t in range(self.T):
@@ -97,7 +103,7 @@ class pendulum():
             H_x2.append(self.x2)
             
             time.append(t+1)
-            H.append(self.x2**2/2 - self.a * math.cos(self.x1) + a)
+            H.append(self.x2**2/2 - self.a * math.cos(self.x1) + self.a)
         
         plt.scatter([self.init_x1], [self.init_x2], c="red", s=20)
         plt.plot(H_x1, H_x2)
@@ -121,7 +127,7 @@ class pendulum():
         time = []
         H = []
         
-        H.append(self.x2**2/2 - self.a * math.cos(self.x1) + a)
+        H.append(self.x2**2/2 - self.a * math.cos(self.x1) + self.a)
         time.append(0)
         
         for t in range(self.T):
@@ -135,7 +141,7 @@ class pendulum():
             H_x2.append(self.x2)
             
             time.append(t+1)
-            H.append((self.x2**2)/2 - self.a * math.cos(self.x1) + a)
+            H.append((self.x2**2)/2 - self.a * math.cos(self.x1) + self.a)
         
         plt.scatter([self.init_x1], [self.init_x2], c="red", s=20)
         plt.scatter([H_x1[0]], [H_x2[0]], c="red", s=20)
@@ -151,6 +157,49 @@ class pendulum():
         
         self.x1 = self.init_x1
         self.x2 = self.init_x2
+        
+    def discrete_gradient(self): #Gonzalez
+        
+        #harmonic oscillator
+        
+        H_x1 = []
+        H_x2 = []
+        
+        time = []
+        H = []
+        
+        H.append(self.x1**2/2 + self.x2**2/2)
+        time.append(0)
+        
+        A = np.array([ [1, -self.epsilon*self.k/(2*self.m)], [self.epsilon*(1/2), 1] ])
+        B = np.array([ [1, self.epsilon*self.k/(2*self.m)], [-self.epsilon*(1/2), 1] ])
+        P = np.linalg.inv(A)@B
+        print(P)
+        x = np.array([self.x1, self.x2])
+        
+        for t in range(self.T):
+            x = P @ x
+            
+            H_x1.append(x[0])
+            H_x2.append(x[1])
+            
+            time.append(t+1)
+            H.append((x[0]**2)/2 + (x[1]**2)/2)
+            
+        plt.scatter([self.init_x1], [self.init_x2], c="red", s=20)
+        plt.plot(H_x1, H_x2)
+        plt.xlabel("x1")
+        plt.ylabel("x2")
+        plt.show()
+        
+        plt.plot(time, H)
+        plt.xlabel("time")
+        plt.ylabel("Hamiltonian")
+        plt.show()
+        
+        self.x1 = self.init_x1
+        self.x2 = self.init_x2
+        
  
 #Pendulum with friction       
 class pendulum_wf():
@@ -165,9 +214,18 @@ class pendulum_wf():
         self.x2 = init_x2
         self.T = T
         
+        self.k = 3
+        self.m = 2
+        
     def pendulum(self,x1,x2):
         x1_dot = x2
         x2_dot = -self.a * math.sin(x1) - self.b * x2
+        
+        return x1_dot, x2_dot
+    
+    def mass_spring(self,x1,x2):
+        x1_dot = x2
+        x2_dot = -x1
         
         return x1_dot, x2_dot
         
@@ -179,7 +237,7 @@ class pendulum_wf():
         time = []
         H = []
         
-        H.append(self.x2**2/2 - self.a * math.cos(self.x1) + a)
+        H.append(self.x2**2/2 - self.a * math.cos(self.x1) + self.a)
         time.append(0)
         
         plt.scatter([self.x1], [self.x2], c="red", s=0.2)
@@ -194,7 +252,7 @@ class pendulum_wf():
             H_x2.append(self.x2)
             
             time.append(t+1)
-            H.append(self.x2**2/2 - self.a * math.cos(self.x1) + a)
+            H.append(self.x2**2/2 - self.a * math.cos(self.x1) + self.a)
         
         plt.scatter([self.init_x1], [self.init_x2], c="red", s=20)
         plt.plot(H_x1, H_x2)
@@ -217,7 +275,7 @@ class pendulum_wf():
         time = []
         H = []
         
-        H.append(self.x2**2/2 - self.a * math.cos(self.x1) + a)
+        H.append(self.x2**2/2 - self.a * math.cos(self.x1) + self.a)
         time.append(0)
         
         for t in range(self.T):
@@ -238,7 +296,7 @@ class pendulum_wf():
             H_x2.append(self.x2)
             
             time.append(t+1)
-            H.append(self.x2**2/2 - self.a * math.cos(self.x1) + a)
+            H.append(self.x2**2/2 - self.a * math.cos(self.x1) + self.a)
         
         plt.scatter([self.init_x1], [self.init_x2], c="red", s=20)
         plt.plot(H_x1, H_x2)
@@ -262,7 +320,7 @@ class pendulum_wf():
         time = []
         H = []
         
-        H.append(self.x2**2/2 - self.a * math.cos(self.x1) + a)
+        H.append(self.x2**2/2 - self.a * math.cos(self.x1) + self.a)
         time.append(0)
         
         for t in range(self.T):
@@ -276,7 +334,7 @@ class pendulum_wf():
             H_x2.append(self.x2)
             
             time.append(t+1)
-            H.append((self.x2**2)/2 - self.a * math.cos(self.x1) + a)
+            H.append((self.x2**2)/2 - self.a * math.cos(self.x1) + self.a)
         
         plt.scatter([self.init_x1], [self.init_x2], c="red", s=20)
         plt.plot(H_x1, H_x2)
@@ -291,7 +349,48 @@ class pendulum_wf():
         
         self.x1 = self.init_x1
         self.x2 = self.init_x2
-
+        
+    def discrete_gradient(self): #Gonzalez
+        
+        #harmonic oscillator
+        
+        H_x1 = []
+        H_x2 = []
+        
+        time = []
+        H = []
+        
+        H.append(self.x1**2/2 + self.x2**2/2)
+        time.append(0)
+        
+        A = np.array([ [1, -self.epsilon*self.k/(2*self.m)], [self.epsilon*(1/2), 1] ])
+        B = np.array([ [1, self.epsilon*self.k/(2*self.m)], [-self.epsilon*(1/2), 1] ])
+        P = np.linalg.inv(A)@B
+        print(P)
+        x = np.array([self.x1, self.x2])
+        
+        for t in range(self.T):
+            x = P @ x
+            
+            H_x1.append(x[0])
+            H_x2.append(x[1])
+            
+            time.append(t+1)
+            H.append((x[0]**2)/2 + (x[1]**2)/2)
+            
+        plt.scatter([self.init_x1], [self.init_x2], c="red", s=20)
+        plt.plot(H_x1, H_x2)
+        plt.xlabel("x1")
+        plt.ylabel("x2")
+        plt.show()
+        
+        plt.plot(time, H)
+        plt.xlabel("time")
+        plt.ylabel("Hamiltonian")
+        plt.show()
+        
+        self.x1 = self.init_x1
+        self.x2 = self.init_x2
 """
 a : param of potential
 b : param of friction
@@ -299,13 +398,15 @@ epsilon : time step width
 init_x2 : initial velue of momentum (mass = 1)
 init_x1 : initial value of position (angle)
 """
-pendulum = pendulum(a=1, epsilon=0.01, init_x1=1, init_x2=1, T=10000)
-pendulum_wf = pendulum_wf(a=1, b=0.01, epsilon=0.01, init_x1=1, init_x2=1, T=10000)
+pendulum = pendulum(a=1, epsilon=0.01, init_x1=1, init_x2=1, T=5000)
+pendulum_wf = pendulum_wf(a=1, b=0.1, epsilon=0.01, init_x1=1, init_x2=1, T=1000)
 
 pendulum.explicit_euler()
 pendulum.runge_kutta_4th() 
-pendulum.symplectic_euler()  
+pendulum.symplectic_euler() 
+pendulum.discrete_gradient() 
 
 pendulum_wf.explicit_euler()
-#pendulum_wf.runge_kutta_4th()
+pendulum_wf.runge_kutta_4th()
 pendulum_wf.symplectic_euler() 
+#pendulum_wf.discrete_gradient()
